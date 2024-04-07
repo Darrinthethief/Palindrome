5cfce291127620407916a8ec502ba0e34fdec0fa
Demar English <demarenglish@Demars-MacBook-Pro.local>
2024-04-07 00:20:40 -0400

    Intital commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const userInput = document.querySelector('#user-input');
+const button = document.querySelector('#button');
+const result = document.querySelector('#result');
+
+const palindromeChecker = userInput =>  {
+    if (userInput === '')  return "Please enter a palindrome!";
+}
