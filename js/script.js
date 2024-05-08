0599a6a0d74a918724f3bcd1651f387d016c217b
Demar English <demarenglish@Demars-MacBook-Pro.local>
2024-05-08 11:57:03 -0400

    Updating interactivity

--- js/script.js
+++ js/script.js
@@ -1,19 +1,30 @@
-const input = document.querySelector("#text-input").value;
-const display = document.querySelector("#result");
-const button = document.querySelector("#check-button");
+const button = document.querySelector('#check-button');
+const resultDisplay = document.querySelector("#result");
 
-function palindromeChecker(word) {
-    const newWord = word.replace(" ", "").split("").reverse().join("").toLowerCase();
+function checkPalindrome() {
+    const userInput = document.querySelector('#text-input').value;
+    const result = document.createElement('p');
+    result.classList.add("palindrome");
 
-    if (newWord === word) {
-        display.innerHTML = "This is a palindrome!";
-    } else {
-        display.innerHTML = "NOT";
+    if (userInput === "") {
+        result.innerHTML = `Please enter a palindrome`;
+        resultDisplay.innerHTML = ""; //
+        resultDisplay.appendChild(result);
+        return;
     }
 
-    display.innerHTML = "";
+    const cleanInput = userInput.replace(/[^A-Za-z0-9]/g, "").toLowerCase();
+    const reversedInput = cleanInput.split("").reverse().join("");
+
+    if (cleanInput === reversedInput) {
+        result.innerHTML = `Yes, "${userInput}" is a palindrome!`;
+    } else {
+        result.innerHTML = `No, "${userInput}" is not a palindrome.`;
+    }
+
+    resultDisplay.innerHTML = ""; //
+    resultDisplay.appendChild(result);
 }
 
-button.addEventListener("click", palindromeChecker(input));
-
+button.addEventListener("click", checkPalindrome);
 
